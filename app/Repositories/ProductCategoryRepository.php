80b706cf4fd39b98e5dcb355bb8d7419de25a5ad
sidar <sidarduygu81@gmail.com>
2023-04-25 00:46:36 +0300

    first commit

---
+++ app/Repositories/ProductCategoryRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\ProductCategory;
+
+class ProductCategoryRepository extends BaseRepository
+{
+    protected $model;
+    public function __construct(ProductCategory $model)
+    {
+        parent::__construct($model);
+    }
+}
